24f5f1d7e15b5d1bed1151a909a118c0e73a138d
Bruno <brufdev@proton.me>
2024-09-07 02:32:18 +0100

    Add Vault policy

---
+++ app/Policies/VaultPolicy.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use App\Models\Vault;
+use Illuminate\Auth\Access\Response;
+
+class VaultPolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, Vault $vault): bool
+    {
+        return $user->id === $vault->created_by;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Vault $vault): bool
+    {
+        return $user->id === $vault->created_by;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, Vault $vault): bool
+    {
+        return $user->id === $vault->created_by;
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, Vault $vault): bool
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, Vault $vault): bool
+    {
+        //
+    }
+}
