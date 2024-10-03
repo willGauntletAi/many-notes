5164b424610edde806ea2b4ad0856141fda3ad91
Bruno <brufdev@proton.me>
2024-10-03 03:00:50 +0100

    Fix menu component markup

--- resources/views/components/menu/item.blade.php
+++ resources/views/components/menu/item.blade.php
@@ -1,10 +1,9 @@
-<div>
+<div class="w-full">
     <button
         class="flex items-center w-full gap-2 px-3 py-1 text-sm text-left transition-colors rounded hover:bg-light-base-400 dark:hover:bg-base-700 text-light-base-950 dark:text-base-50"
         {{ $attributes->merge([
             'type' => 'button',
-        ]) }}
-    >
+        ]) }}>
         {{ $slot }}
     </button>
 </div>
--- resources/views/components/menu/items.blade.php
+++ resources/views/components/menu/items.blade.php
@@ -1,9 +1,4 @@
-<div
-    x-show="menuOpen"
-    x-anchor.bottom-end="$refs.button"
-    @click.away="menuOpen = false"
-    class="min-w-[10rem] z-20 border bg-light-base-200 dark:bg-base-950 border-light-base-300 dark:border-base-500 divide-y rounded-md shadow-lg p-1.5 outline-nonea"
-    x-cloak
->
+<div x-show="menuOpen" x-anchor.bottom-end="$refs.button" @click.away="menuOpen = false" x-cloak
+    class="min-w-[10rem] z-10 border bg-light-base-200 dark:bg-base-950 border-light-base-300 dark:border-base-500 divide-y rounded-md shadow-lg p-1.5 outline-nonea">
     {{ $slot }}
 </div>
