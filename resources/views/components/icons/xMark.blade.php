6253083e0ea54b1af447c7933d6c7a47e05f2f61
Bruno <brufdev@proton.me>
2024-10-03 03:13:16 +0100

    Add missing close icon

---
+++ resources/views/components/icons/xMark.blade.php
@@ -0,0 +1,4 @@
+<svg {{ $attributes }} data-slot="icon" aria-hidden="true" fill="none" stroke-width="1.5" stroke="currentColor"
+    viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+    <path d="M6 18 18 6M6 6l12 12" stroke-linecap="round" stroke-linejoin="round"></path>
+</svg>
