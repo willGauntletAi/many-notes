fb03d647f18642e97ef2013457104c5d731b48de
Bruno <brufdev@proton.me>
2024-10-03 03:46:35 +0100

    Limit allowed characters used in vault names

--- app/Livewire/Forms/VaultForm.php
+++ app/Livewire/Forms/VaultForm.php
@@ -4,14 +4,26 @@ namespace App\Livewire\Forms;
 
 use Livewire\Form;
 use App\Models\Vault;
+use Illuminate\Support\Str;
 use Livewire\Attributes\Validate;
 
 class VaultForm extends Form
 {
     public ?Vault $vault = null;
 
-    #[Validate('required|min:3')]
+    #[Validate]
     public $name = '';
+
+    public function rules(): array
+    {
+        return [
+            'name' => [
+                'required',
+                'min:3',
+                'regex:/^[\s\w.-]+$/',
+            ],
+        ];
+    }
 
     public function setVault(Vault $vault): void
     {
@@ -19,17 +31,22 @@ class VaultForm extends Form
         $this->name = $vault->name;
     }
 
-    public function create()
+    public function create(): void
     {
+        $this->validate();
+
+        $this->name = Str::trim($this->name);
         auth()->user()->vaults()->create([
             'name' => $this->name,
         ]);
-
         $this->reset(['name']);
     }
 
-    public function update()
+    public function update(): void
     {
+        $this->validate();
+
+        $this->name = Str::trim($this->name);
         $this->vault->update([
             'name' => $this->name,
         ]);
