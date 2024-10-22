23f8f17d3a74d40fd0d0ed89e6fbb6390c3b91d5
Bruno <brufdev@proton.me>
2024-10-22 15:12:18 +0100

    Moved js to Alpine component Fix display of webp images Limit size of content's area

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,4 @@
 import './bootstrap';
+import { marked } from 'marked';
+
+window.marked = marked;
--- resources/views/livewire/vault/show.blade.php
+++ resources/views/livewire/vault/show.blade.php
@@ -16,15 +16,8 @@
     </x-layouts.appHeader>
 
     <x-layouts.appMain>
-        <div x-data="{
-            isSidebarOpen: false,
-            isEditMode: $wire.entangle('isEditMode'),
-            selectedFile: $wire.entangle('selectedFile'),
-            html: '',
-            toggleEditMode() { this.isEditMode = !this.isEditMode },
-        }" x-init="$watch('isEditMode', value => html = markdown())
-        $watch('selectedFile', value => html = markdown())" x-cloak
-            @sidebar-left-toggle.window="isSidebarOpen = !isSidebarOpen" class="relative flex w-full">
+        <div x-data="vault" x-cloak @sidebar-left-toggle.window="isSidebarOpen = !isSidebarOpen"
+            class="relative flex w-full">
             <div wire:loading wire:target.except="nodeForm.name, nodeForm.content"
                 class="fixed inset-0 z-40 opacity-50 bg-base-950"></div>
             <div x-show="isSidebarOpen && window.innerWidth < 768" @click="isSidebarOpen = false"
@@ -93,80 +86,83 @@
 
             <div class="absolute top-0 bottom-0 right-0 flex flex-col w-full overflow-y-auto transition-all text-start md:pl-60"
                 :class="{ 'md:pl-60': isSidebarOpen, '': !isSidebarOpen }">
-                @if ($selectedFile)
-                    <div class="sticky top-0 z-[5] p-4 bg-light-base-50 dark:bg-base-900">
-                        <div class="flex justify-between">
-                            <input type="text" wire:model.live.debounce.500ms="nodeForm.name"
-                                class="flex flex-grow p-0 pr-2 text-lg bg-transparent border-0 focus:ring-0 focus:outline-0" />
+                <div class="flex flex-col h-full w-[48rem] mx-auto">
+                    @if ($selectedFile)
+                        <div class="sticky top-0 z-[5] p-4 bg-light-base-50 dark:bg-base-900">
+                            <div class="flex justify-between">
+                                <input type="text" wire:model.live.debounce.500ms="nodeForm.name"
+                                    class="flex flex-grow p-0 pr-2 text-lg bg-transparent border-0 focus:ring-0 focus:outline-0" />
 
-                            <div class="flex items-center gap-2">
-                                <span wire:loading.flex wire:target="nodeForm.name, nodeForm.content"
-                                    class="flex items-center">
-                                    <x-icons.spinner class="w-4 h-4 animate-spin" />
-                                </span>
+                                <div class="flex items-center gap-2">
+                                    <span wire:loading.flex wire:target="nodeForm.name, nodeForm.content"
+                                        class="flex items-center">
+                                        <x-icons.spinner class="w-4 h-4 animate-spin" />
+                                    </span>
 
-                                @if ($nodeForm->extension == 'md')
-                                    <button type="button" x-show="isEditMode" @click="toggleEditMode"
-                                        title="{{ __('Click to read') }}">
-                                        <x-icons.bookOpen class="w-5 h-5" />
+                                    @if ($nodeForm->extension == 'md')
+                                        <button type="button" x-show="isEditMode" @click="toggleEditMode"
+                                            title="{{ __('Click to read') }}">
+                                            <x-icons.bookOpen class="w-5 h-5" />
+                                        </button>
+                                        <button type="button" x-show="!isEditMode" @click="toggleEditMode"
+                                            title="{{ __('Click to edit') }}">
+                                            <x-icons.codeBracket class="w-5 h-5" />
+                                        </button>
+                                    @endif
+
+                                    <button type="button" wire:click="closeFile" title="{{ __('Close file') }}">
+                                        <x-icons.xMark class="w-5 h-5" />
                                     </button>
-                                    <button type="button" x-show="!isEditMode" @click="toggleEditMode"
-                                        title="{{ __('Click to edit') }}">
-                                        <x-icons.codeBracket class="w-5 h-5" />
-                                    </button>
-                                @endif
-
-                                <button type="button" wire:click="closeFile" title="{{ __('Close file') }}">
-                                    <x-icons.xMark class="w-5 h-5" />
-                                </button>
+                                </div>
                             </div>
+
+                            @error('nodeForm.name')
+                                <p class="text-sm text-error-500" aria-live="assertive">{{ $message }}</p>
+                            @enderror
                         </div>
+                        <div class="flex flex-grow px-4">
+                            @if ($nodeForm->extension == 'md')
+                                <textarea wire:model.live.debounce.500ms="nodeForm.content" x-show="isEditMode" id="noteEdit"
+                                    data-id="{{ $selectedFile }}" class="w-full h-full p-0 bg-transparent border-0 focus:ring-0 focus:outline-0"></textarea>
 
-                        @error('nodeForm.name')
-                            <p class="text-sm text-error-500" aria-live="assertive">{{ $message }}</p>
-                        @enderror
-                    </div>
-                    <div class="flex flex-grow px-4">
-                        @if ($nodeForm->extension == 'md')
-                            <textarea wire:model.live.debounce.500ms="nodeForm.content" x-show="isEditMode" id="noteEdit"
-                                data-id="{{ $selectedFile }}" class="w-full h-full p-0 bg-transparent border-0 focus:ring-0 focus:outline-0"></textarea>
+                                <div x-show="!isEditMode" x-html="html" id="noteView"
+                                    class="w-full h-full markdown-body">
+                                </div>
+                            @elseif (in_array($nodeForm->extension, ['webp', 'png', 'jpg', 'jpeg', 'gif']))
+                                <div>
+                                    <img src="{{ $selectedFilePath }}" />
+                                </div>
+                            @elseif (in_array($nodeForm->extension, ['pdf']))
+                                <object type="application/pdf" data="{{ $selectedFilePath }}"
+                                    class="w-full h-full"></object>
+                            @elseif (in_array($nodeForm->extension, ['mp4', 'avi']))
+                                <video class="w-full" controls>
+                                    <source src="{{ $selectedFilePath }}" />
+                                    {{ __('Your browser does not support the video tag') }}
+                                </video>
+                            @elseif (in_array($nodeForm->extension, ['mp3', 'flac']))
+                                <div class="flex items-start justify-center w-full">
+                                    <audio class="w-full" controls>
+                                        <source src="{{ $selectedFilePath }}">
+                                        {{ __('Your browser does not support the audio tag') }}
+                                    </audio>
+                                </div>
+                            @endif
+                        </div>
+                    @else
+                        <div class="flex items-center justify-center w-full h-full gap-2">
+                            <x-form.button @click="$wire.dispatchTo('modals.search-node', 'open-modal')">
+                                <x-icons.magnifyingGlass class="w-4 h-4" />
+                                <span class="hidden text-sm font-medium md:block">{{ __('Open file') }}</span>
+                            </x-form.button>
 
-                            <div x-show="!isEditMode" x-html="html" id="noteView" class="w-full h-full markdown-body">
-                            </div>
-                        @elseif (in_array($nodeForm->extension, ['jpg', 'jpeg', 'png', 'gif']))
-                            <div>
-                                <img src="{{ $selectedFilePath }}" />
-                            </div>
-                        @elseif (in_array($nodeForm->extension, ['pdf']))
-                            <object type="application/pdf" data="{{ $selectedFilePath }}"
-                                class="w-full h-full"></object>
-                        @elseif (in_array($nodeForm->extension, ['webp', 'mp4', 'avi']))
-                            <video class="w-full" controls>
-                                <source src="{{ $selectedFilePath }}" />
-                                {{ __('Your browser does not support the video tag') }}
-                            </video>
-                        @elseif (in_array($nodeForm->extension, ['mp3', 'flac']))
-                            <div class="flex items-start justify-center w-full">
-                                <audio class="w-full" controls>
-                                    <source src="{{ $selectedFilePath }}">
-                                    {{ __('Your browser does not support the audio tag') }}
-                                </audio>
-                            </div>
-                        @endif
-                    </div>
-                @else
-                    <div class="flex items-center justify-center w-full h-full gap-2">
-                        <x-form.button @click="$wire.dispatchTo('modals.search-node', 'open-modal')">
-                            <x-icons.magnifyingGlass class="w-4 h-4" />
-                            <span class="hidden text-sm font-medium md:block">{{ __('Open file') }}</span>
-                        </x-form.button>
-
-                        <x-form.button primary @click="$wire.dispatchTo('modals.add-node', 'open-modal')">
-                            <x-icons.plus class="w-4 h-4" />
-                            <span class="hidden text-sm font-medium md:block">{{ __('New note') }}</span>
-                        </x-form.button>
-                    </div>
-                @endif
+                            <x-form.button primary @click="$wire.dispatchTo('modals.add-node', 'open-modal')">
+                                <x-icons.plus class="w-4 h-4" />
+                                <span class="hidden text-sm font-medium md:block">{{ __('New note') }}</span>
+                            </x-form.button>
+                        </div>
+                    @endif
+                </div>
             </div>
         </div>
     </x-layouts.appMain>
@@ -176,63 +172,97 @@
     <livewire:modals.search-node :$vault />
 </div>
 
-@assets
-    <script src="https://cdn.jsdelivr.net/npm/marked/marked.min.js"></script>
-@endassets
+@script
+    <script>
+        Alpine.data('vault', () => ({
+            isSidebarOpen: false,
+            isEditMode: $wire.entangle('isEditMode'),
+            selectedFile: $wire.entangle('selectedFile'),
+            html: '',
+            renderListitem: null,
 
-<script>
-    let markedRender = new marked.Renderer;
-    markedRender.parser = new marked.Parser;
-    let renderListitem = markedRender.listitem.bind(markedRender);
+            toggleEditMode() {
+                this.isEditMode = !this.isEditMode;
+            },
 
-    function markdown() {
-        let el = document.getElementById('noteEdit');
-        let markdown = '';
+            init() {
+                this.$watch('isEditMode', value => {
+                    if (value) {
+                        return;
+                    }
+                    this.html = this.markdownToHtml();
+                });
 
-        if (!el) {
-            return markdown;
-        }
+                this.$watch('selectedFile', value => {
+                    if (value === null) {
+                        this.html = '';
+                        return;
+                    }
+                    this.html = this.markdownToHtml();
+                });
 
-        renderer = {
-            image(token) {
-                // external images
-                if (token.href.startsWith('http://') || token.href.startsWith('https://')) {
-                    return '<img src="' + token.href + '" alt="' + token.text + '" />';
+                let markedRender = new marked.Renderer;
+                markedRender.parser = new marked.Parser;
+                this.renderListitem = markedRender.listitem.bind(markedRender);
+            },
+
+            markdownToHtml() {
+                let el = document.getElementById('noteEdit');
+                let markdown = '';
+                let renderListitem = this.renderListitem;
+                let node = this.selectedFile;
+
+                if (!el) {
+                    return markdown;
                 }
 
-                // internal images
-                return '<img src="/files/{{ $vault->id }}?path=' + token.href + '&node=' + node + '" alt="' +
-                    token.text + '" />';
+                renderer = {
+                    image(token) {
+                        let html = '';
+
+                        if (token.href.startsWith('http://') || token.href.startsWith('https://')) {
+                            // external images
+                            html = '<img src="' + token.href + '" alt="' + token.text + '" />';
+                        } else {
+                            // internal images
+                            html = '<img src="/files/{{ $vault->id }}?path=' + token.href + '&node=' +
+                                node + '" alt="' +
+                                token.text + '" />';
+                        }
+
+                        return '<span class="flex items-center justify-center">' + html + '</span>';
+                    },
+                    link(token) {
+                        // external links
+                        if (token.href.startsWith('http://') || token.href.startsWith('https://')) {
+                            return '<a href="' + token.href + '" title="' + (token.title ?? '') +
+                                '" target="_blank">' +
+                                token.text + '</a>';
+                        }
+
+                        // internal links
+                        return '<a href="" wire:click.prevent="openFilePath(\'' + token.href +
+                            '\')" title="' + (token
+                                .title ?? '') + '">' + token.text +
+                            '</a>';
+                    },
+                    listitem(token) {
+                        let html = renderListitem(token);
+
+                        if (token.task) {
+                            html = html.replace('<li>', '<li class="task-list-item">');
+                            html = html.replace('<input ', '<input class="task-list-item-checkbox" ');
+                        }
+
+                        return html;
+                    },
+                };
+                marked.use({
+                    renderer
+                });
+
+                return marked.parse(el.value);
             },
-            link(token) {
-                // external links
-                if (token.href.startsWith('http://') || token.href.startsWith('https://')) {
-                    return '<a href="' + token.href + '" title="' + (token.title ?? '') + '" target="_blank">' +
-                        token
-                        .text + '</a>';
-                }
-
-                // internal links
-                return '<a href="" wire:click.prevent="openFilePath(\'' + token.href + '\')" title="' + (token
-                        .title ?? '') + '">' + token.text +
-                    '</a>';
-            },
-            listitem(token) {
-                let html = renderListitem(token);
-
-                if (token.task) {
-                    html = html.replace('<li>', '<li class="task-list-item">');
-                    html = html.replace('<input ', '<input class="task-list-item-checkbox" ');
-                }
-
-                return html;
-            },
-        };
-        marked.use({
-            renderer
-        });
-        markdown = marked.parse(el.value);
-
-        return markdown;
-    }
-</script>
+        }))
+    </script>
+@endscript
