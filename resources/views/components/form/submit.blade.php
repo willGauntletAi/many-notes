e3bd64ac480292f2f1463e854c5290652558b4da
Bruno <brufdev@proton.me>
2024-10-24 19:47:14 +0100

    Fix submit button's text color

--- resources/views/components/form/submit.blade.php
+++ resources/views/components/form/submit.blade.php
@@ -5,7 +5,7 @@
 
 <button
     type="submit"
-    class="relative flex w-full justify-center rounded-md bg-primary-400 dark:bg-primary-500 hover:bg-primary-300 dark:hover:bg-primary-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm focus-visible:outline focus-visible:outline-1 focus-visible:outline-offset-2 focus-visible:outline-light-base-600 dark:focus-visible:outline-base-400 disabled:cursor-not-allowed disabled:opacity-75"
+    class="relative flex w-full justify-center rounded-md bg-primary-400 dark:bg-primary-500 hover:bg-primary-300 dark:hover:bg-primary-600 text-light-base-50 px-3 py-1.5 text-sm font-semibold leading-6 shadow-sm focus-visible:outline focus-visible:outline-1 focus-visible:outline-offset-2 focus-visible:outline-light-base-600 dark:focus-visible:outline-base-400 disabled:cursor-not-allowed disabled:opacity-75"
 >
     {{ $label }}
 
