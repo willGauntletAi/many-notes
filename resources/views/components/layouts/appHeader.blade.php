eda89c9d6332b8c0dc6a8a950a9cc61a3f2ca8cd
Bruno <brufdev@proton.me>
2024-10-27 15:58:31 +0000

    Limit app width to better fit big screens

--- resources/views/components/layouts/appHeader.blade.php
+++ resources/views/components/layouts/appHeader.blade.php
@@ -1,6 +1,6 @@
 <header
     class="border-b bg-light-base-100 dark:bg-base-800 text-light-base-700 dark:text-base-200 border-light-base-300 dark:border-base-500">
-    <div class="md:container md:mx-auto">
+    <div class="max-w-[63rem] mx-auto">
         <div class="flex justify-between px-4 py-5">
             {{ $slot }}
         </div>
--- resources/views/components/layouts/appMain.blade.php
+++ resources/views/components/layouts/appMain.blade.php
@@ -1,5 +1,5 @@
 <main class="flex flex-grow bg-light-base-100 dark:bg-base-800 text-light-base-950 dark:text-base-50">
-    <div class="flex flex-grow md:container md:mx-auto bg-light-base-50 dark:bg-base-900">
+    <div class="flex flex-grow max-w-[63rem] mx-auto bg-light-base-50 dark:bg-base-900">
         {{ $slot }}
     </div>
 </main>
