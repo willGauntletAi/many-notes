1cb9858456091b4206fd13f440cd96a8f947e718
Bruno <brufdev@proton.me>
2024-11-03 02:48:14 +0000

    Fix Livewire to read max filesize from ENV var

--- config/livewire.php
+++ config/livewire.php
@@ -65,7 +65,10 @@ return [
 
     'temporary_file_upload' => [
         'disk' => null,        // Example: 'local', 's3'              | Default: 'default'
-        'rules' => ['file', 'mimes:zip', 'max:102400'],       // Example: ['file', 'mimes:png,jpg']  | Default: ['required', 'file', 'max:12288'] (12MB)
+        'rules' => [
+            'file',
+            'max:' . Monolog\Utils::expandIniShorthandBytes(ini_get('upload_max_filesize')),
+        ],       // Example: ['file', 'mimes:png,jpg']  | Default: ['required', 'file', 'max:12288'] (12MB)
         'directory' => null,   // Example: 'tmp'                      | Default: 'livewire-tmp'
         'middleware' => null,  // Example: 'throttle:5,1'             | Default: 'throttle:60,1'
         'preview_mimes' => [   // Supported file types for temporary pre-signed file URLs...
