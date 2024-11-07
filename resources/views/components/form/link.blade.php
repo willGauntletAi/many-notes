0b65dbcdad50187df7ffb7578c46fd82fa68d448
Bruno <brufdev@proton.me>
2024-11-07 20:40:13 +0000

    Fix Tailwind classes and remove unnecessary ones

--- resources/views/components/form/link.blade.php
+++ resources/views/components/form/link.blade.php
@@ -1,6 +1,4 @@
-<a
-    {{ $attributes }}
-    class="text-sm text-primary-400 dark:text-primary-500 hover:text-primary-300 dark:text-hover-primary-600"
->
+<a {{ $attributes }}
+    class="text-sm text-primary-400 dark:text-primary-500 hover:text-primary-300 dark:hover:text-primary-600">
     {{ $slot }}
 </a>
--- resources/views/components/menu/items.blade.php
+++ resources/views/components/menu/items.blade.php
@@ -1,4 +1,4 @@
 <div x-show="menuOpen" x-anchor.bottom-end="$refs.button" @click.away="menuOpen = false" x-cloak
-    class="min-w-[10rem] z-10 border bg-light-base-200 dark:bg-base-950 border-light-base-300 dark:border-base-500 divide-y rounded-md shadow-lg p-1.5 outline-nonea">
+    class="min-w-[10rem] z-10 border bg-light-base-200 dark:bg-base-950 border-light-base-300 dark:border-base-500 rounded-md shadow-lg p-1.5">
     {{ $slot }}
 </div>
