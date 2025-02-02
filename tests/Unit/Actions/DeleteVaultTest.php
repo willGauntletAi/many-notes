558a34c646eaa7c0e41305ea06cfe45fd2e408ad
brufdev <brufdev@proton.me>
2025-02-02 18:40:43 +0000

    Remove unused test

--- tests/Unit/Actions/DeleteVaultTest.php
+++ tests/Unit/Actions/DeleteVaultTest.php
@@ -5,23 +5,6 @@ declare(strict_types=1);
 use App\Actions\DeleteVault;
 use App\Models\User;
 
-/*
-it('throw an exception', function (): void {
-    $user = User::factory()->hasVaults(1)->create();
-    $vault = $user->vaults()->first();
-
-    //DB::shouldReceive('beginTransaction')
-    //    ->andThrow(new Exception());
-    Mockery::mock(new Vault())
-        ->makePartial()
-        ->shouldReceive('nodes')
-        ->andThrow(new Exception());
-
-    //expect(new DeleteVault()->handle($vault))
-    //    ->toThrow(Exception::class);
-});
-*/
-
 it('deletes a vault without a folder in the disk', function (): void {
     $user = User::factory()->hasVaults(1)->create();
     $vault = $user->vaults()->first();
