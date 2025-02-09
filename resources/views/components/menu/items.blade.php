dea32551073d91195dae949a0af276cc5e27f312
brufdev <brufdev@proton.me>
2025-02-09 16:59:38 +0000

    Add minor CSS fix

--- resources/views/components/menu/items.blade.php
+++ resources/views/components/menu/items.blade.php
@@ -1,4 +1,4 @@
 <div x-show="menuOpen" x-anchor.bottom-end="$refs.button" @click.away="menuOpen = false" x-cloak
-    class="min-w-[10rem] z-10 border bg-light-base-200 dark:bg-base-950 border-light-base-300 dark:border-base-500 rounded-md shadow-lg p-1.5 text-light-base-950 dark:text-base-50">
+    class="min-w-[10rem] z-[35] border bg-light-base-200 dark:bg-base-950 border-light-base-300 dark:border-base-500 rounded-md shadow-lg p-1.5 text-light-base-950 dark:text-base-50">
     {{ $slot }}
 </div>
