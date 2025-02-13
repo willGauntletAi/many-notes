ab63a9326df0442e60883bdb670188e80bb8e34d
brufdev <brufdev@proton.me>
2025-02-13 13:40:47 +0000

    Enforce the same database structure across all different engines

--- database/migrations/2024_12_17_132832_add_templates_node_id_to_vaults_table.php
+++ database/migrations/2024_12_17_132832_add_templates_node_id_to_vaults_table.php
@@ -14,9 +14,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('vaults', function (Blueprint $table): void {
-            $table->after('name', function (Blueprint $table): void {
-                $table->foreignId('templates_node_id')->nullable()->constrained('vault_nodes')->nullOnDelete();
-            });
+            $table->foreignId('templates_node_id')->nullable()->constrained('vault_nodes')->nullOnDelete();
         });
     }
 };
